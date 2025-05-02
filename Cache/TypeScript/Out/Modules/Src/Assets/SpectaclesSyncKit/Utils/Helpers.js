fc68b0d7d71266390e66b004073852ad41cbfff2
reatured <61943376+reatured@users.noreply.github.com>
2025-05-02 19:42:52 -0400

    Initiate again

--- Cache/TypeScript/Out/Modules/Src/Assets/SpectaclesSyncKit/Utils/Helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNullOrUndefined = void 0;
-function isNullOrUndefined(value) {
-    return value === null || value === undefined;
-}
-exports.isNullOrUndefined = isNullOrUndefined;
-//# sourceMappingURL=Helpers.js.map
--- Cache/TypeScript/StoredResults/Modules/Src/Assets/SpectaclesSyncKit/Mapping/src/Texts/MessageTextsEnum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MessageTextsEnum = void 0;
-var MessageTextsEnum;
-(function (MessageTextsEnum) {
-    MessageTextsEnum["USER_ALIGNED"] = "USER_ALIGNED";
-})(MessageTextsEnum || (exports.MessageTextsEnum = MessageTextsEnum = {}));
-//# sourceMappingURL=MessageTextsEnum.js.map
